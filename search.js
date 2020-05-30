0ab988b5524e785370e0c849f585953f37d1171f
Jennifer Rosa <rosajen27@gmail.com>
2020-05-30 14:42:53 -0400

    updated js

--- search.js
+++ search.js
@@ -5,38 +5,42 @@ $("#search-button").on("click", function(event) {
 // grab text from the search-input box
 var movie = $("#search-input").val();
 
-// hit the queryURL with $ajax
-//take response data and display it in the console log
-// response will return an array with all movies matching searched title
+// hit the queryURL with $ajax, response will return an array with movies matching searched title
 var queryURL = "http://www.omdbapi.com/?s=" + movie + "&apikey=3814d304"
 
 $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function(response) {
-
-    console.log(response);
     
-      // storing the data from the AJAX request in the results variable
+      // Store the data from the AJAX request
       let results = response.Search;
 
       // Looping through each result item
-      // Appending search results
       results.forEach(function(movie){
 
-        $("#movie-list").append("<h1>" + movie.Title + "</h1> <br>" + "<img src='" + movie.Poster + "'> <br>" + movie.Year + "<br> Rated: " + movie.Rated + "<br> Genre: " + movie.Genre + "<br> Plot: " + movie.Plot + "<br> <hr>");
-        
-        // to do:
-        // take imdbID in order to display Rated, Genre, Plot, & Ratings Array (Source + Value)
+        // Obtain movieIDs in order to call another API
         var movieID = movie.imdbID;
         console.log(movieID);
 
+        // Second API
+        var queryURL2 = "http://www.omdbapi.com/?i=" + movieID + "&apikey=3814d304"
+        $.ajax({
+          url: queryURL2,
+          method: "GET",
+          success: function(data) {
+          console.log(data);
+
+          $("#movie-list").append("<h1>" + data.Title + "</h1> <br>" + "<img src='" + data.Poster + "'> <br>" + data.Released + "<br> Rated: " + data.Rated + "<br> Genre: " + data.Genre + "<br> Plot: " + data.Plot + "<br> <hr>");
+ 
+          }
+        });
 
       });
 
 });
 });
+       
 
-
-
-
+// to do:
+// display review arrays
