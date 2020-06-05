51d1defa578039d023a5ca4e9b6747b4741e71d8
Jake S <bearcoco5@gmail.com>
2020-06-04 20:46:09 -0400

    Added Promise API

--- index.js
+++ index.js
@@ -5,30 +5,34 @@ var movies = [];
 
 function getIMDbObj(movie) {
     var queryURL = OMDB + movie + OMDBkey;
-    return $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://imdb8.p.rapidapi.com/title/get-overview-details?currentCountry=US&tconst=" + response.imdbID,
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "imdb8.p.rapidapi.com",
-                "x-rapidapi-key": "38519610ffmsh90a3c30c45c5dbbp178690jsn61a4c17f6c68"
+
+    var promise = new Promise( function(resolve, reject) {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://imdb8.p.rapidapi.com/title/get-overview-details?currentCountry=US&tconst=" + response.imdbID,
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "imdb8.p.rapidapi.com",
+                    "x-rapidapi-key": "38519610ffmsh90a3c30c45c5dbbp178690jsn61a4c17f6c68"
+                }
             }
-        }
-        $.ajax(settings).then(function (response) {
-            console.log(response);
-        });
-    });
+            $.ajax(settings).then(function (response) {
+                resolve(response);
+            });
+        }); 
+    }); 
+    return promise;
 
 }
 
 
 function formatMovie(obj) {
-    return form = {
+    form = {
         title: obj.title.title,
         runtime: obj.title.runningTimeInMinutes,
         release: obj.title.year,
@@ -42,73 +46,56 @@ function formatMovie(obj) {
 
 
     }
+    return form;
 
 }
 
 //Use this to get an object filled with movie info
 function makeMovObj(movie) {
-    let m;
-    getIMDbObj(movie).done(function (response) { m = formatMovie(response); });
-    return m;
+
+    var promise = new Promise(function (resolve, reject) {
+        getIMDbObj(movie).then(function (i) {
+            resolve(formatMovie(i));
+        });
+    });
+    return promise;
 }
 
 
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 function displayMovieInfo() {
 
+    console.log("Clicked on a movie");
 
+    /**  YOUR CODE GOES HERE!!!
+     * 
+     * deatals drop down when a Poster is clicked
+     * 
+     * 
+    var holder = $("#movies-view");
+    holder.empty();
 
-    var movie = $(this).attr("data-name");
+    var rd = $("<h5>");
+    rd.text(response.Released);
+    holder.append(rd);
 
-    var queryURL = OMDB + movie + OMDBkey;
-
-
-    // Creates AJAX call for the specific movie button being clicked
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        // YOUR CODE GOES HERE!!!
-        var holder = $("#movies-view");
-        holder.empty();
-
-        var im = $("<img>");
-        im.attr("src", response.Poster);
-        holder.append(im);
-
-        var rate = $("<h4>");
-        rate.text(response.Rated);
-        holder.append(rate);
-
-        var rateing = $("<h4>");
-        var ratings = response.Ratings;
-        console.log(ratings);
-        var rating;
-        for (var r = 0; r < ratings.length; r++) {
-            if (ratings[r].Source === "Internet Movie Database") {
-                rating = ratings[r].Value;
-            }
-        }
-
-        rateing.text(rating);
-        holder.append(rateing);
-
-        var rd = $("<h5>");
-        rd.text(response.Released);
-        holder.append(rd);
-
-        var pl = $("<h2>");
-        pl.text(response.Plot);
-        holder.append(pl);
-
-    });
+    var pl = $("<h2>");
+    pl.text(response.Plot);
+    holder.append(pl);
+*/
 
 }
 
 // Function for displaying movie data
 function renderButtons() {
 
+    /** 
+     * Need more work on how we are going to 
+     * propogate. Have first result be the closest to
+     * the search query, follow by movies in same genre,
+     * cascade down most relavent search results. Potentialy
+     * add filters?
+     * 
     let genreTag = "#" + $(this).attr("data-genre");
     // Deletes the movies prior to adding new movies
     // (this is necessary otherwise you will have repeat buttons)
@@ -130,15 +117,48 @@ function renderButtons() {
         // Added the button to the buttons-view div
         $(genreTag).append(a);
     }
+
+    Following is placeholder to help development
+    */
+
+
+    for (var i = 0; i < movies.length; i++) {
+        var movieObj;
+        makeMovObj(movies[i]).then(function (result) {
+            console.dir(result);
+            movieObj = result
+        });
+
+
+        let genreTag = "#ActionAdventure"; //+ movieObj.genres[0];
+        // Deletes the movies prior to adding new movies
+        // (this is necessary otherwise you will have repeat buttons)
+        $(genreTag).empty();
+
+        console.log("movies " + movies);
+        console.log("movie obj " + movieObj);
+
+        var a = $("carousel-cell");
+        a.attr("data-name", movies[i]);
+        // Adds a class of movie to our button
+        a.addClass("movie");
+        a.css("background-image", `url(${movieObj.poster})`);
+        // Added a data-attribute
+        a.attr("data-name", movies[i]);
+        a.attr("data-genre", movieObj.genres);
+        // Added the button to the buttons-view div
+        $(genreTag).append(a);
+    }
 }
 
 // This function handles events where the add movie button is clicked
 $("#movie-search").on("click", function (event) {
     event.preventDefault();
+    console.log("Clicked search");
     // This line of code will grab the input from the textbox
     var movie = $("#movie-input").val().trim();
 
-    // The movie from the textbox is then added to our array
+    // First movie in Movies is the searched movie; movies[0] == search query
     movies.push(movie);
 
     // Calling renderButtons which handles the processing of our movie array
