d72e8f3b603be1ff9539c87925f2dc044cfd12c5
Jake S <bearcoco5@gmail.com>
2020-06-06 13:33:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $("#search-button").on("click", function (event) {
     var movie = $("#search-input").val();
 
     // hit the queryURL with $ajax, response will return an array with movies matching searched title
-    var queryURL = "http://www.omdbapi.com/?s=" + movie + "&apikey=3814d304"
+    var queryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=3814d304"
 
     $.ajax({
         url: queryURL,
@@ -26,7 +26,7 @@ $("#search-button").on("click", function (event) {
             console.log(movieID);
 
             // Second API
-            var queryURL2 = "http://www.omdbapi.com/?i=" + movieID + "&apikey=3814d304"
+            var queryURL2 = "https://www.omdbapi.com/?i=" + movieID + "&apikey=3814d304"
             $.ajax({
                 url: queryURL2,
                 method: "GET",
